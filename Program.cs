96c131f0217b776b502eeb342f0903f271f45f61
Herma <49307226+hermaplusplus@users.noreply.github.com>
2021-10-15 22:19:07 +0100

    User Info command added

--- Program.cs
+++ Program.cs
@@ -61,6 +61,54 @@ namespace Medea
             {
                 message.Channel.SendMessageAsync($@"Your account was created at {message.Author.CreatedAt.DateTime.Date}");
             }
+            else if (command.Equals("info") || command.Equals("user") || command.Equals("userinfo"))
+            {
+                if (message.MentionedUsers.Count > 0)
+                {
+                    IEnumerator<SocketUser> enummentions = message.MentionedUsers.GetEnumerator();
+                    for (int i = 0; i < message.MentionedUsers.Count; i++)
+                    {
+                        enummentions.MoveNext();
+
+                        EmbedBuilder emb = new EmbedBuilder
+                        {
+                            Title = $@"User Info for {enummentions.Current.Mention}"
+                        };
+
+                        emb
+                            .AddField("Username & Tag", $@"{enummentions.Current.Username}#{enummentions.Current.Discriminator}", true)
+                            .AddField("User ID", $@"{enummentions.Current.Id}", true)
+                            .AddField("Avatar URL", $@"[Click Me!]({enummentions.Current.GetAvatarUrl()})", true)
+                            .AddField("Date & Time Created", $@"<t:{enummentions.Current.CreatedAt.ToUnixTimeSeconds()}:F>")
+                            .WithThumbnailUrl(enummentions.Current.GetAvatarUrl())
+                            .WithAuthor(message.Author as IUser)
+                            .WithCurrentTimestamp();
+
+                        UserProperties userProperties = enummentions.Current.PublicFlags.Value;
+                        string flagList = "";
+                        if (!userProperties.HasFlag(UserProperties.None))
+                        {
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.BugHunterLevel1) ? "Bug Hunter Level 1,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.BugHunterLevel2) ? "Bug Hunter Level 2,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.DiscordCertifiedModerator) ? "Certified Moderator,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.EarlySupporter) ? "Early Supporter,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.EarlyVerifiedBotDeveloper) ? "Early Verified Bot Dev,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.HypeSquadBalance) ? "HypeSquad Balance,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.HypeSquadBravery) ? "HypeSquad Bravery,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.HypeSquadBrilliance) ? "Hypesquad Brilliance,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.HypeSquadEvents) ? "HypeSquad Event Member,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.Partner) ? "Discord Partner Server Owner,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.Staff) ? "Discord Employee,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.System) ? "Discord System,\n" : "");
+                            String.Concat(flagList, userProperties.HasFlag(UserProperties.VerifiedBot) ? "Verified Bot,\n" : "");
+                            emb.AddField("Public Flags", flagList);
+                        }
+
+                        message.Channel.SendMessageAsync(null, false, emb.Build(), null, null, message.Reference);
+                    }
+                }
+
+            }
             else if ((command.Equals("meta") || command.Equals("metadata")) && message.Reference != null)
             {
                 IMessage refmsg = message.Channel.GetMessageAsync(message.Reference.MessageId.Value).Result;
@@ -89,7 +137,7 @@ Size:     {(enumattach.Current.Size / (1024.0 * 1024)).ToString("0.##")}MB
         private Task JoinHandler(SocketGuildUser user)
         {
             user.SendMessageAsync($@"Welcome to **{user.Guild.Name}**!");
-            
+
             if (File.Exists($@"./res/guildjoin/{user.Guild.Id}.txt"))
             {
                 user.SendMessageAsync(File.ReadAllText($@"./res/guildjoin/{user.Guild.Id}.txt"));
